f27b59977c1729abac62f80a33135e56cbc3b4b4
Tung Hoang <tungjty@gmail.com>
2024-05-22 16:03:08 +0700

    code chapter 12

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,7 @@
-export default function Page() {
+export default async function Page() {
+  console.log('Loading customers page...');
+  await new Promise((resolve) => setTimeout(resolve, 3000));
+  console.log('Customers page completed after 3 seconds.');
+
   return <p>Customers Page</p>;
 }
