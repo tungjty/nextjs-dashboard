47d4e40ac1d8dba07d10879e9ea76a05a18025f5
Tung Hoang <tungjty@gmail.com>
2025-07-06 21:46:42 +0700

    commit Chapter 12 code

--- app/ui/skeletons.tsx
+++ app/ui/skeletons.tsx
@@ -32,7 +32,7 @@ export function CardsSkeleton() {
 export function RevenueChartSkeleton() {
   return (
     <div className={`${shimmer} relative w-full overflow-hidden md:col-span-4`}>
-      <div className="mb-4 h-8 w-36 rounded-md bg-gray-100" />
+      <div className="mb-4 h-8 w-36 rounded-md bg-gray-200" />
       <div className="rounded-xl bg-gray-100 p-4">
         <div className="sm:grid-cols-13 mt-0 grid h-[410px] grid-cols-12 items-end gap-2 rounded-md bg-white p-4 md:gap-4" />
         <div className="flex items-center pb-2 pt-6">
